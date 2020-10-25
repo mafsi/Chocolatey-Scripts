7cbef5a9fba7d9180ff322d26e55b71473fc83b9
Patrick Danilevici <61430691+mafsi@users.noreply.github.com>
2020-10-25 21:13:18 +0200

    adding some programs

--- install-choco-script.bat
+++ install-choco-script.bat
@@ -1,6 +1,8 @@
 :: Install choco .exe and add choco to PATH
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+choco install dependencies -y
+
 :: Install all the packages
 :::: Browsers
 choco install firefox -fy
